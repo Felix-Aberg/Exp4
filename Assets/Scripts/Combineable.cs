9d69bda98d6773ac8166978fa7015507c342de5e
Felix Åberg <felix.aberg.2773@student.uu.se>
2021-03-04 14:50:19 +0100

    the game
    that's it

---
+++ Assets/Scripts/Combineable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combineable : MonoBehaviour
+{
+    public int level;
+    public bool combining;
+
+    public GameObject combineInto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  
+    {
+        Debug.Log("ta");
+        if (CompareTag(collision.gameObject.tag))
+        {
+            Debug.Log("tag");
+            if(collision.gameObject.GetComponent<Combineable>().level == level
+                && !combining)
+            {
+                Debug.Log("tag 2!");
+                Combine(collision);
+            }
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Combine(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Combineable>().combining = true;
+        Vector3 pos = Vector3.Lerp(transform.position, collision.gameObject.transform.position, 0.5f);
+        GameObject obj = Instantiate(combineInto, transform.parent);
+        obj.transform.position = pos;
+
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    bool holding;
+    Vector3 mousePosition;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("rsst");
+        holding = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (holding)
+        {
+            //collider.size = colliderSize;
+            holding = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        if (holding)
+            Hold();
+    }
+
+    void Hold()
+    {
+        Vector3 deltaMouse = mousePosition; // + offset;
+        Debug.Log("rs");
+        //Sensitivity
+        transform.position = deltaMouse;
+    }
+}
