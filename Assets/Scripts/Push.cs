50ee6f96eedf2ba7adb2498dbde209286ed9a61f
Felix Åberg <felix.aberg.2773@student.uu.se>
2021-03-09 15:17:58 +0100

    better walls, balanced pushing
    now to fix spawns

---
+++ Assets/Scripts/Push.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public float strength;
+
+    void OnTriggerStay2D(UnityEngine.Collider2D other)
+    {
+        Debug.Log("AAA");
+        Vector3 force = Vector3.zero;
+        force.y = -other.transform.position.y;
+        other.transform.GetComponent<Rigidbody2D>().AddForce(force * strength);
+    }
+}
